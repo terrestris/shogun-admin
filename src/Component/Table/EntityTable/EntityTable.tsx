477918bd053f64025dd908005b57a1f8fe7d99a4
Kai Volland <kaivolland@yahoo.de>
2020-10-08 15:30:51 +0200

    Introduces EntityTable

---
+++ src/Component/Table/EntityTable/EntityTable.tsx
@@ -0,0 +1,250 @@
+import React, { useState } from 'react';
+
+import { EditableCell } from '../../Table/EditableCell';
+
+import Table, { ColumnsType, TableProps } from 'antd/lib/table';
+import TableUtil from '../../../Util/TableUtil';
+import { Form, Modal, Input, notification, Tooltip } from 'antd';
+
+import {
+  CheckCircleTwoTone,
+  CloseCircleOutlined,
+  EditOutlined,
+  DeleteOutlined
+} from '@ant-design/icons';
+
+import './EntityTable.less';
+import { matchPath, useHistory, useLocation } from 'react-router-dom';
+import BaseEntity from '../../../Model/BaseEntity';
+import GenericService from '../../../Service/GenericService/GenericService';
+
+export type EntityTableColumn = ColumnsType<BaseEntity> & {
+  editable: boolean;
+  dataIndex: string;
+  title: string;
+};
+
+type OwnProps= {
+  disableActions?: boolean;
+  service: any;
+  routePath: string;
+  columns?: EntityTableColumn | EntityTableColumn[];
+  name: {
+    singular: string;
+    plural: string;
+  };
+};
+
+export type EntityTableProps = OwnProps & TableProps<BaseEntity>;
+
+const defaultColumns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: TableUtil.getSorter('name'),
+    defaultSortOrder: 'ascend',
+    editable: true,
+    ...TableUtil.getColumnSearchProps('name')
+  }
+];
+
+export const EntityTable: React.FC<EntityTableProps> = ({
+  disableActions = false,
+  service,
+  routePath,
+  name,
+  columns = defaultColumns,
+  ...passThroughProps
+}) => {
+
+  const entityService: GenericService<BaseEntity> = service;
+
+  const history = useHistory();
+  const location = useLocation();
+  const match = matchPath<{entityId: string}>(location.pathname, {
+    path: `${routePath}/:entityId`
+  });
+  const entityId = match?.params?.entityId;
+
+  const [entities, setEntities] = useState<BaseEntity[]>();
+  const [editingId, setEditingId] = useState('');
+  const [loadingState, setLoadingState] = useState<'failed' | 'loading' | 'done'>();
+  const [form] = Form.useForm();
+
+  const fetchEntities = async () => {
+    setLoadingState('loading');
+    try {
+      const fetchedEntities = await entityService.findAll();
+      setEntities(fetchedEntities);
+      setLoadingState('done');
+    } catch (error) {
+      setLoadingState('failed');
+    }
+  };
+
+  if (!loadingState) {
+    fetchEntities();
+  }
+
+  const isEditing = (record: BaseEntity) => record.id === editingId;
+
+  const edit = (record: BaseEntity) => {
+    form.setFieldsValue({
+      ...record
+    });
+    setEditingId(record.id);
+  };
+
+  const onDeleteClick = (record) => {
+    if (!record.id) {
+      setEntities(entities.filter(r => r !== record));
+      return;
+    }
+
+    let confirmName;
+    Modal.confirm({
+      cancelText: 'Abbrechen',
+      title: `${name.singular} löschen`,
+      content: (
+        <div>
+          <div>Die {name.singular} wird gelöscht!</div>
+          <br />
+          <div>Bitte geben sie zum Bestätigen den Namen ein:</div>
+          <Input onChange={(e) => { confirmName = e.target.value; }} />
+        </div>
+      ),
+      onOk: () => {
+        if (confirmName === record.name) {
+          entityService
+            .delete(record.id)
+            .then(() => {
+              notification.info({
+                message: `${name.singular} gelöscht`,
+                description: `${name.singular} "${record.name}" wurde gelöscht`
+              });
+              fetchEntities();
+            });
+        }
+      }
+    });
+  };
+
+  const save = async (id: string) => {
+    const row = (await form.validateFields()) as BaseEntity;
+    const affectedEntity = entities.find(t => t.id === id);
+    const updatedEntity = {
+      ...affectedEntity,
+      ...row
+    };
+
+    if (affectedEntity.id) {
+      entityService
+        .update(updatedEntity)
+        .then(fetchEntities)
+        .then(stopEditing);
+    } else {
+      entityService
+        .add(updatedEntity)
+        .then(fetchEntities)
+        .then(stopEditing);
+    }
+  };
+
+  const stopEditing = () => {
+    setEditingId('');
+  };
+
+  const tableColumns = [
+    ...columns
+  ];
+
+  if(!disableActions) {
+    tableColumns.push({
+      title: '',
+      className: 'operation-column',
+      width: 100,
+      dataIndex: 'operation',
+      render: (_: any, record: BaseEntity) => {
+        if (isEditing(record)) {
+          return (
+            <div className="actions">
+              <Tooltip title="Speichern">
+                <CheckCircleTwoTone
+                  twoToneColor="#52c41a"
+                  onClick={() => save(record.id)}
+                  style={{ marginRight: 8 }}
+                />
+              </Tooltip>
+              <Tooltip title="Abbrechen">
+                <CloseCircleOutlined onClick={stopEditing} />
+              </Tooltip>
+            </div>
+          );
+        } else {
+          return (
+            <div className="actions">
+              <Tooltip title="Bearbeiten">
+                <EditOutlined onClick={() => edit(record)} />
+              </Tooltip>
+              <Tooltip title="Löschen">
+                <DeleteOutlined onClick={() => onDeleteClick(record)} />
+              </Tooltip>
+            </div>
+          );
+        }
+      }
+    });
+  }
+
+  const onRowClick = (record: BaseEntity, event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    history.push(`${routePath}/${record.id}`);
+  };
+
+  return (
+    <Form form={form} component={false}>
+      <Table
+        className="entity-table"
+        rowSelection={{
+          type: 'radio',
+          columnWidth: 0,
+          renderCell: () => '',
+          selectedRowKeys: entityId ? [entityId.toString()] : []
+        }}
+        rowKey={(record)=> record.id.toString()}
+        rowClassName={() => 'editable-row'}
+        dataSource={entities}
+        pagination={false}
+        components={{
+          body: {
+            cell: EditableCell
+          }
+        }}
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => onRowClick(record, event)
+          };
+        }}
+        columns={
+          tableColumns.map((col: EntityTableColumn) => {
+            if (!col.editable) {
+              return col;
+            }
+            return {
+              ...col,
+              onCell: record => ({
+                record,
+                dataType: 'text',
+                editing: isEditing(record),
+                dataIndex: col.dataIndex,
+                title: col.title
+              })
+            };
+          })
+        }
+        {...passThroughProps}
+      />
+    </Form>
+  );
+};
+
+export default EntityTableColumn;
