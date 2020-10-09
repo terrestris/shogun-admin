d628d54c0c7addc835289c9329d90f042026a2ca
Kai Volland <kaivolland@yahoo.de>
2020-10-09 11:56:55 +0200

    Use EntityTable in ApplicationTable

--- src/Component/Application/ApplicationTable/ApplicationTable.tsx
+++ src/Component/Application/ApplicationTable/ApplicationTable.tsx
@@ -1,174 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import Application from '../../../Model/Application';
-
-import Table, { TableProps } from 'antd/lib/table';
-import TableUtil from '../../../Util/TableUtil';
-import { Form, Modal, Input, notification, Tooltip } from 'antd';
-
-import {
-  DeleteOutlined
-} from '@ant-design/icons';
-
-import './ApplicationTable.less';
+import { EntityTable, EntityTableProps } from '../../../Component/Table/EntityTable/EntityTable';
 import ApplicationService from '../../../Service/ApplicationService/ApplicationService';
-import { matchPath, useHistory, useLocation } from 'react-router-dom';
+import TableUtil from '../../../Util/TableUtil';
 
-type OwnProps = {
-  disableActions?: boolean;
-};
+interface OwnProps { }
 
-type ApplicationTableProps = OwnProps & TableProps<Application>;
+type ApplicationTableProps = OwnProps & Omit<EntityTableProps, 'service' | 'routePath' | 'name' | 'columns' >;
+const apploicationService = new ApplicationService();
 
-const applicationService = new ApplicationService();
-
-export const ApplicationTable: React.FC<ApplicationTableProps> = ({
-  disableActions = false,
-  ...passThroughProps
-}) => {
-
-  const history = useHistory();
-  const location = useLocation();
-  const match = matchPath<{ applicationId: string }>(location.pathname, {
-    path: '/portal/application/:applicationId'
-  });
-  const applicationId = match?.params?.applicationId;
-
-  const [applications, setApplications] = useState<Application[]>();
-  const [loadingState, setLoadingState] = useState<'failed' | 'loading' | 'done'>();
-  const [form] = Form.useForm();
-
-  const fetchApplications = async () => {
-    setLoadingState('loading');
-    try {
-      const apps = await applicationService.findAll();
-      setApplications(apps);
-      setLoadingState('done');
-    } catch (error) {
-      setLoadingState('failed');
-    }
-  };
-
-  if (!loadingState) {
-    fetchApplications();
+const columns: any = [
+  {
+    title: 'Name',
+    key: 'name',
+    dataIndex: 'name',
+    sorter: TableUtil.getSorter('name'),
+    defaultSortOrder: 'ascend',
+    editable: true,
+    ...TableUtil.getColumnSearchProps('name')
   }
+];
 
-  /**
-   * Update application list after a certain application settings were changed
-   */
-  useEffect(() => {
-    if (!applicationId && loadingState) {
-      fetchApplications();
-    }
-  }, [applicationId]);
-
-  const handleSearch = (selectedKeys, confirm) => {
-    confirm();
-  };
-
-  const handleReset = clearFilters => {
-    clearFilters();
-  };
-
-  const onDeleteClick = (record) => {
-    if (!record.id) {
-      setApplications(applications.filter(r => r !== record));
-      return;
-    }
-
-    let confirmName;
-    Modal.confirm({
-      cancelText: 'Abbrechen',
-      title: 'Applikation löschen',
-      content: (
-        <div>
-          <div>Die Applikation wird gelöscht!</div>
-          <br />
-          <div>Bitte geben sie zum Bestätigen den Namen ein:</div>
-          <Input onChange={(e) => { confirmName = e.target.value; }} />
-        </div>
-      ),
-      onOk: () => {
-        if (confirmName === record.name) {
-          applicationService
-            .delete(record.id)
-            .then(() => {
-              notification.info({
-                message: 'Applikation gelöscht',
-                description: `Applikation "${record.name}" wurde gelöscht`
-              });
-              fetchApplications();
-            });
-        }
-      }
-    });
-  };
-
-  const getSorter = (index: string) => {
-    return (a, b) => {
-      a = typeof a[index] === 'string' ? a[index]?.toLowerCase() : a[index]?.toString();
-      b = typeof b[index] === 'string' ? b[index]?.toLowerCase() : b[index]?.toString();
-      return a?.localeCompare(b) || 0;
-    };
-  };
-
-  const columns: any[] = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      sorter: getSorter('name'),
-      defaultSortOrder: 'ascend',
-      ...TableUtil.getColumnSearchProps('name', handleSearch, handleReset)
-    }
-  ];
-
-  if (!disableActions) {
-    columns.push({
-      title: '',
-      className: 'operation-column',
-      width: 100,
-      dataIndex: 'operation',
-      render: (_: any, record: Application) => {
-        return (
-          <div className="actions">
-            <Tooltip title="Löschen">
-              <DeleteOutlined onClick={() => onDeleteClick(record)} />
-            </Tooltip>
-          </div>
-        );
-      }
-    });
-  }
-
-  const onRowClick = (record: Application, event: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    if (event.target instanceof SVGElement) {
-      return;
-    }
-    history.push(`/portal/application/${record.id}`);
-  };
+export const ApplicationTable: React.FC<ApplicationTableProps> = props => {
 
   return (
-    <Form form={form} component={false}>
-      <Table
-        className="application-table"
-        rowSelection={{
-          type: 'radio',
-          columnWidth: 0,
-          renderCell: () => '',
-          selectedRowKeys: applicationId ? [applicationId.toString()] : []
-        }}
-        rowKey={(record) => record.id.toString()}
-        dataSource={applications}
-        pagination={false}
-        onRow={(record: Application, rowIndex: number) => {
-          return {
-            onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => onRowClick(record, event)
-          };
-        }}
-        columns={columns}
-        {...passThroughProps}
-      />
-    </Form>
+    <EntityTable
+      service={apploicationService}
+      routePath={'/portal/application'}
+      name={{
+        singular: 'Applikation',
+        plural: 'Applilkationen'
+      }}
+      columns={columns}
+      actions={['delete']}
+      {...props}
+    />
   );
 };
 
