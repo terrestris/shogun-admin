58f8d7b67fcf5f0e600727e80a637513bdc8738b
Kai Volland <kaivolland@yahoo.de>
2020-10-09 09:39:03 +0200

    Removes logout method from LayerService

--- src/Service/LayerSerivce/LayerService.ts
+++ src/Service/LayerSerivce/LayerService.ts
@@ -1,18 +1,12 @@
 import GenericService from '../GenericService/GenericService';
 
 import config from 'shogunApplicationConfig';
-import { keycloak } from '../../Util/KeyCloakUtil';
-import { KeycloakPromise } from 'keycloak-js';
 import Layer from '../../Model/Layer';
 
 class LayerService extends GenericService<Layer> {
 
   constructor() {
     super(Layer, config.path.layer);
-  }
-
-  static logout(): KeycloakPromise<void, void> {
-    return keycloak.logout();
   }
 
 }
