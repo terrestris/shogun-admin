d65c705532efabbacb04acddebd331b9206c09a6
Daniel Koch <koch@terrestris.de>
2021-03-11 14:49:38 +0100

    Make visibility of the dashboard items configurable and remove the logs entry

--- src/Component/WelcomeDashboard/WelcomeDashboard.tsx
+++ src/Component/WelcomeDashboard/WelcomeDashboard.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
 
-import './WelcomeDashboard.less';
+import {
+  Col,
+  Row,
+  Statistic,
+  List
+} from 'antd';
+
+import Avatar from 'antd/lib/avatar/avatar';
 
 import {
   AppstoreOutlined,
@@ -9,23 +17,24 @@ import {
   DashboardOutlined,
   LikeOutlined,
   MailOutlined,
-  UnorderedListOutlined,
   UserOutlined
 } from '@ant-design/icons';
 
-import { Col, Row, Statistic, List } from 'antd';
 import { Dashboard } from '../Dashboard/Dashboard';
 import { DashboardCard } from '../Dashboard/DashboardCard/DashboardCard';
 import ApplicationTable from '../Application/ApplicationTable/ApplicationTable';
-import Avatar from 'antd/lib/avatar/avatar';
 import LayerTable from '../Layer/LayerTable/LayerTable';
 import UserTable from '../User/UserTable/UserTable';
 
 import config from 'shogunApplicationConfig';
 
+import './WelcomeDashboard.less';
+
 type WelcomeDashboardProps = {};
 
 export const WelcomeDashboard: React.FC<WelcomeDashboardProps> = (props) => {
+
+  const dashboardConf = config.dashboard;
 
   return (
     <Dashboard
@@ -33,75 +42,82 @@ export const WelcomeDashboard: React.FC<WelcomeDashboardProps> = (props) => {
       title={'Dashboard'}
       columns={3}
     >
-      <DashboardCard
-        className="news-dashboard-card"
-        title="Nachrichten"
-        description="… was habe ich verpasst"
-        avatar={<MailOutlined />}
-        hoverable={false}
-      >
-        <List
-          itemLayout="horizontal"
-          dataSource={[]}
-          renderItem={item => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<a href="https://ant.design">{item.title}</a>}
-                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              />
-            </List.Item>
-          )}
-        />
-      </DashboardCard>
-      <DashboardCard
-        className="statistics-dashboard-card"
-        title="Statistiken"
-        description="… wie siehts denn aus"
-        avatar={<DashboardOutlined />}
-        hoverable={false}
-      >
-        <Row gutter={16}>
-          <Col span={12}>
-            <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
-          </Col>
-          <Col span={12}>
-            <Statistic title="Unmerged" value={93} suffix="/ 100" />
-          </Col>
-        </Row>
-      </DashboardCard>
-      <DashboardCard
-        className="application-dashboard-card"
-        title={<Link to={`${config.appPrefix}/portal/logs`}>Logs</Link>}
-        description="… die die Welt erklären"
-        avatar={<Link to={`${config.appPrefix}/portal/logs`}><UnorderedListOutlined /></Link>}
-      >
-        {/* <UserProfileForm /> */}
-      </DashboardCard>
-      <DashboardCard
-        className="application-dashboard-card"
-        title={<Link to={`${config.appPrefix}/portal/application`}>Applikationen</Link>}
-        description="… die die Welt verändern"
-        avatar={<Link to={`${config.appPrefix}/portal/application`}><BankOutlined /></Link>}
-      >
-        <ApplicationTable />
-      </DashboardCard>
-      <DashboardCard
-        className="layer-dashboard-card"
-        title={<Link to={`${config.appPrefix}/portal/layer`}>Themen</Link>}
-        description="… die die Welt bewegen"
-        avatar={<Link to={`${config.appPrefix}/portal/layer`}><AppstoreOutlined /></Link>}
-      >
-        <LayerTable />
-      </DashboardCard>
-      <DashboardCard
-        className="layer-dashboard-card"
-        title={<Link to={`${config.appPrefix}/portal/user`}>User</Link>}
-        description="… die die Welt verbessern"
-        avatar={<Link to={`${config.appPrefix}/portal/user`}><UserOutlined /></Link>}
-      >
-        <UserTable />
-      </DashboardCard>
+      {
+        dashboardConf?.news?.visible &&
+          <DashboardCard
+            className="news-dashboard-card"
+            title="Nachrichten"
+            description="… was habe ich verpasst"
+            avatar={<MailOutlined />}
+            hoverable={false}
+          >
+            <List
+              itemLayout="horizontal"
+              dataSource={[]}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                    title={<a href="https://ant.design">{item.title}</a>}
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  />
+                </List.Item>
+              )}
+            />
+          </DashboardCard>
+      }
+      {
+        dashboardConf?.statistics?.visible &&
+          <DashboardCard
+            className="statistics-dashboard-card"
+            title="Statistiken"
+            description="… wie siehts denn aus"
+            avatar={<DashboardOutlined />}
+            hoverable={false}
+          >
+            <Row gutter={16}>
+              <Col span={12}>
+                <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
+              </Col>
+              <Col span={12}>
+                <Statistic title="Unmerged" value={93} suffix="/ 100" />
+              </Col>
+            </Row>
+          </DashboardCard>
+      }
+      {
+        dashboardConf?.applications?.visible &&
+          <DashboardCard
+            className="application-dashboard-card"
+            title={<Link to={`${config.appPrefix}/portal/application`}>Applikationen</Link>}
+            description="… die die Welt verändern"
+            avatar={<Link to={`${config.appPrefix}/portal/application`}><BankOutlined /></Link>}
+          >
+            <ApplicationTable />
+          </DashboardCard>
+      }
+      {
+        dashboardConf?.layers?.visible &&
+          <DashboardCard
+            className="layer-dashboard-card"
+            title={<Link to={`${config.appPrefix}/portal/layer`}>Themen</Link>}
+            description="… die die Welt bewegen"
+            avatar={<Link to={`${config.appPrefix}/portal/layer`}><AppstoreOutlined /></Link>}
+          >
+            <LayerTable />
+          </DashboardCard>
+      }
+      {
+        dashboardConf?.users?.visible &&
+          <DashboardCard
+            className="layer-dashboard-card"
+            title={<Link to={`${config.appPrefix}/portal/user`}>User</Link>}
+            description="… die die Welt verbessern"
+            avatar={<Link to={`${config.appPrefix}/portal/user`}><UserOutlined /></Link>}
+          >
+            <UserTable />
+          </DashboardCard>
+      }
     </Dashboard>
   );
 };
