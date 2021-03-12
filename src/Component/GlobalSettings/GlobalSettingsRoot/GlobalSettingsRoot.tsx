b47a010517f0a33920804207bd8449cb76891405
Daniel Koch <koch@terrestris.de>
2021-03-12 16:08:19 +0100

    Add global settings panel

---
+++ src/Component/GlobalSettings/GlobalSettingsRoot/GlobalSettingsRoot.tsx
@@ -0,0 +1,39 @@
+import React, {
+  useEffect
+} from 'react';
+
+import { useHistory } from 'react-router-dom';
+
+import {
+  PageHeader
+} from 'antd';
+
+import EvictCache from '../../../Component/EvictCache/EvictCache';
+
+import './GlobalSettingsRoot.less';
+
+type LogsProps = {};
+
+export const GlobalSettings: React.FC<LogsProps> = (props) => {
+
+  const history = useHistory();
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div
+      className="global-settings-root"
+    >
+      <PageHeader
+        className="header"
+        onBack={() => history.goBack()}
+        title="Einstellungen"
+        subTitle="… die die Welt lenken"
+      />
+      <EvictCache />
+    </div>
+  );
+};
+
+export default GlobalSettings;
