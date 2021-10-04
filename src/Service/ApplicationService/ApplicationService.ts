1970c1e076f1970f7a301040d4ae3118696aadba
Andre Henn <henn@terrestris.de>
2021-10-04 11:35:57 +0200

    fix: use correct model in application service

--- src/Service/ApplicationService/ApplicationService.ts
+++ src/Service/ApplicationService/ApplicationService.ts
@@ -1,5 +1,4 @@
 import GenericService from '../GenericService/GenericService';
-import User from '../../Model/User';
 
 import config from 'shogunApplicationConfig';
 import Application from '../../Model/Application';
@@ -7,7 +6,7 @@ import Application from '../../Model/Application';
 class ApplicationService extends GenericService<Application> {
 
   constructor() {
-    super(User, config.path.application);
+    super(Application, config.path.application);
   }
 
 }
