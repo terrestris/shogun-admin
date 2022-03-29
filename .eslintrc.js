3113113b8eb23115799ac2905216c909f6951810
Kai Volland <volland@terrestris.de>
2022-03-29 17:19:07 +0200

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: '@terrestris/eslint-config-typescript'
+  extends: '@terrestris/eslint-config-typescript-react',
+  rules: {
+    '@typescript-eslint/member-ordering': 'off',
+    'object-property-newline': 'error',
+    'object-curly-newline': 'error'
+  }
 };
