8f76167f1592cd3a4ec3131891621cadeeed51d8
Kai Volland <kaivolland@yahoo.de>
2022-04-07 09:06:28 +0200

    fix: remove dangling comma
    Co-authored-by: Anna Rieger <rieger@terrestris.de>

--- src/Util/SecurityUtil.ts
+++ src/Util/SecurityUtil.ts
@@ -4,7 +4,7 @@ export default class SecurityUtil {
 
   public static getSecurityHeaders(config: any): any {
     const securityHeaders: any = {
-      'X-XSRF-TOKEN': CsrfUtil.getCsrfValueFromCookie(),
+      'X-XSRF-TOKEN': CsrfUtil.getCsrfValueFromCookie()
     };
     if (config?.security?.jwt === true) {
       securityHeaders.AUTHORIZATION = localStorage.getItem(config?.security?.authTokenKey);
