ff2f6beed3bf7e6ec07b2be425199c96ab836e45
arieger <rieger@terrestris.de>
2022-04-29 14:35:07 +0200

    feat: introduce markdown editor field

---
+++ src/Component/FormField/MarkdownEditor/MarkdownEditor.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+export type MarkdownEditorProps = {
+  value?: string;
+  onChange?: (value: string) => void;
+};
+
+export const MarkdownEditor: React.FC<MarkdownEditorProps> = ({
+  value,
+  onChange
+}) => {
+  const [markdown, setMarkdown] = React.useState<string>();
+
+  useEffect(() => {
+    setMarkdown(value);
+  }, [value]);
+
+  const onMarkdownChange = (val: string) => {
+    onChange(val);
+  };
+
+  return (
+    <MDEditor
+      value={markdown}
+      onChange={onMarkdownChange}
+    />
+  );
+};
+
+export default MarkdownEditor;
