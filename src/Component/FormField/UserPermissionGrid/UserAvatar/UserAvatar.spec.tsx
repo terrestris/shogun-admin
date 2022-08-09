b5dbb4b9e0dd8fe22da77eecb4be0becb235b794
Daniel Koch <koch@terrestris.de>
2022-08-09 14:42:05 +0200

    feat: add the UserPermissionGrid to configure the UserInstancePermissions for a given entity

---
+++ src/Component/FormField/UserPermissionGrid/UserAvatar/UserAvatar.spec.tsx
@@ -0,0 +1,9 @@
+import UserAvatar from './UserAvatar';
+
+describe('<UserAvatar />', () => {
+
+  it('is defined', () => {
+    expect(UserAvatar).not.toBeUndefined();
+  });
+
+});
---
+++ src/Component/FormField/UserPermissionGrid/UserPermissionGrid.spec.tsx
@@ -0,0 +1,9 @@
+import UserPermissionGrid from './UserPermissionGrid';
+
+describe('<UserPermissionGrid />', () => {
+
+  it('is defined', () => {
+    expect(UserPermissionGrid).not.toBeUndefined();
+  });
+
+});
