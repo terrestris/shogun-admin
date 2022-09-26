125fdd7158e85cccad2fd13ff6bb1c6550c81c07
Rui Nunes <rnunes@terrestris.de>
2022-09-26 20:13:36 +0200

    fix: updated ApplicationInfo test

--- src/Component/Modal/ApplicationInfo/ApplicationInfo.spec.tsx
+++ src/Component/Modal/ApplicationInfo/ApplicationInfo.spec.tsx
@@ -6,6 +6,12 @@ import ApplicationInfo from './ApplicationInfo';
 
 const recoilInitializer = (snap) => snap.set(shogunInfoModalVisibleAtom, true);
 
+const PROJECT_VERSION = '1.0.0';
+
+jest.mock('../../../Hooks/useClientVersion', () => ({
+  useClientVersion: () => PROJECT_VERSION
+}));
+
 describe('<ApplicationInfo />', () => {
 
   test('Modal has a title', async () => {
@@ -24,6 +30,12 @@ describe('<ApplicationInfo />', () => {
     render(<ApplicationInfo />, recoilInitializer);
     const versionInfo = await screen.getByText('SHOGun Version');
     expect(versionInfo).toBeDefined();
+  });
+
+  test('Modal contains Admin-Client-Version-Info', async () => {
+    render(<ApplicationInfo />, recoilInitializer);
+    const adminClientVersionInfo = await screen.getByText('Admin Version');
+    expect(adminClientVersionInfo).toBeDefined();
   });
 
 });
