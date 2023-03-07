b81f1195856c2a00eb18c386e8f002003fea10b6
Fritz Hoeing <fhoeing@terrestris.de>
2023-03-07 17:33:54 +0100

    feat: init playwright

---
+++ playwright.config.ts
@@ -0,0 +1,70 @@
+import {
+  defineConfig
+} from '@playwright/test';
+
+export default defineConfig({
+  // @ts-ignore
+  globalSetup: require.resolve('./global-setup.ts'),
+  testDir: './e2e-tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  // @ts-ignore
+  forbidOnly: !!process.env.CI,
+  retries: 4,
+  // @ts-ignore
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', {
+    open: 'never'
+  }]],
+  use: {
+    // @ts-ignore
+    baseURL: process.env.HOST,
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+    permissions: ['geolocation'],
+
+    ignoreHTTPSErrors: true,
+
+    viewport: {
+      width: 800,
+      height: 600
+    }
+  },
+
+  projects: [
+    { name: 'setup',
+      testMatch: /.*\.setup\.ts/ },
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+        locale: 'de-DE'
+      },
+      dependencies: ['setup']
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        locale: 'de-DE'
+      },
+      dependencies: ['setup']
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        browserName: 'webkit',
+        locale: 'de-DE'
+      },
+      dependencies: ['setup']
+    }
+  ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: './e2e-tests/test-results/'
+});
