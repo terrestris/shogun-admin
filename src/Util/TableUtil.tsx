499f74f95eb3510d27d77ef482dfe13aa60be7a6
faouziH21 <2004homsani@gmail.com>
2024-03-05 13:26:49 +0100

    feat: reset button actually resets search field

--- src/Util/TableUtil.tsx
+++ src/Util/TableUtil.tsx
@@ -55,7 +55,10 @@ export default class TableUtil {
             >
               {`${i18n.t('GeneralEntityTable.popupFilter')}`}
             </Button>
-            <Button onClick={() => handleReset(clearFilters)} size="small" style={{ flex: 1 }}>
+            <Button
+              onClick={() => {handleReset(clearFilters); handleSearch(selectedKeys, confirm);}}
+              size="small"
+              style={{ flex: 1 }}>
               {`${i18n.t('GeneralEntityTable.popupReset')}`}
             </Button>
           </div>
