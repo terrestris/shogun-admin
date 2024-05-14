f3c42776cb7676a9ee52923dcc4fce813be09478
sdobbert <dobbert@terrestris.de>
2024-05-14 08:26:07 +0200

    fix: remove unnecessairy variables

--- global-setup.ts
+++ global-setup.ts
@@ -7,10 +7,6 @@ async function globalSetup(config: FullConfig) {
   process.env.ID = '20';
   // @ts-ignore
   process.env.HOST = 'https://shogun2022.intranet.terrestris.de';
-  // @ts-ignore
-  process.env.ADMIN_LOGIN = 'shogun';
-  // @ts-ignore
-  process.env.ADMIN_PASSWORD = 'shogun';
 }
 
 export default globalSetup;
