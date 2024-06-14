345fb2bd5085414521d7f3a88cd76c91e9965503
Kai Volland <kaivolland@yahoo.de>
2024-06-14 11:37:13 +0200

    test: remove some slow and failing fake tests

--- src/Component/GeneralEntity/GeneralEntityForm/GeneralEntityForm.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { GeneralEntityForm } from './GeneralEntityForm';
-
-describe('<GeneralEntityForm />', () => {
-
-  it('is defined', () => {
-    expect(GeneralEntityForm).not.toBeUndefined();
-  });
-
-});
