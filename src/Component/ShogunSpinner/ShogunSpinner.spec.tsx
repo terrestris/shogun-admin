eef8aff85e14edb3eeff1abb0ca6b5facbc011c4
sdobbert <dobbert@terrestris.de>
2024-07-09 17:15:06 +0200

    test: test if shogun spinner can be rendered

--- src/Component/ShogunSpinner/ShogunSpinner.spec.tsx
+++ src/Component/ShogunSpinner/ShogunSpinner.spec.tsx
@@ -1,7 +1,26 @@
+import React from 'react';
+
+import {
+  render,
+  screen
+} from '@testing-library/react';
+
 import { ShogunSpinner } from './ShogunSpinner';
 
 describe('<ShogunSpinner />', () => {
   it('is defined', () => {
     expect(ShogunSpinner).not.toBeUndefined();
+  });
+
+  it('can be rendered', () => {
+    const {
+      container
+    } = render(<ShogunSpinner />);
+
+    expect(container).toBeVisible();
+
+    const imageElement = screen.getByAltText('shogun spinner');
+    expect(imageElement).toBeVisible();
+    expect(imageElement).toHaveAttribute('src', 'test-file-stub');
   });
 });
