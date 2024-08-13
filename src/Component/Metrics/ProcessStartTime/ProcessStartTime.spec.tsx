9ca5954e7477c5df7b527c3359d4343348c2ad88
sdobbert <dobbert@terrestris.de>
2024-08-13 16:09:19 +0200

    test: update unittest for ProcessStartTime

--- src/Component/Metrics/ProcessStartTime/ProcessStartTime.spec.tsx
+++ src/Component/Metrics/ProcessStartTime/ProcessStartTime.spec.tsx
@@ -1,4 +1,19 @@
+import React from 'react';
+
+import {
+  render,
+  screen,
+  fireEvent,
+} from '@testing-library/react';
+
 import { ProcessStartTime } from './ProcessStartTime';
+import moment from 'moment';
+
+jest.mock('../MetricEntry/MetricEntry', () => jest.fn(({ valueRenderer }) => (
+  <div data-testid="metric-entry">
+    {valueRenderer(1627840800)} { }
+  </div>
+)));
 
 describe('<ProcessStartTime />', () => {
 
@@ -6,4 +21,17 @@ describe('<ProcessStartTime />', () => {
     expect(ProcessStartTime).not.toBeUndefined();
   });
 
+  it('can be rendered and formats the start time', () => {
+    const {
+      container
+    } = render(
+      <ProcessStartTime />
+    );
+    expect(container).toBeVisible();
+
+    expect(screen.getByTestId('metric-entry')).toBeInTheDocument();
+
+    const expectedDate = moment(1627840800 * 1000).format('llll');
+    expect(screen.getByText(expectedDate)).toBeInTheDocument();
+  });
 });
