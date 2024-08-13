513c55ec4ec9d0687a7b806552191eb1860f5c78
sdobbert <dobbert@terrestris.de>
2024-08-13 16:27:01 +0200

    test: update unittests for SystemCpuCount, SystemCpuUsage, and SystemLoadAverage

--- src/Component/Metrics/SystemLoadAverage/SystemLoadAverage.spec.tsx
+++ src/Component/Metrics/SystemLoadAverage/SystemLoadAverage.spec.tsx
@@ -1,9 +1,28 @@
+import React from 'react';
+
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+
 import { SystemLoadAverage } from './SystemLoadAverage';
 
+jest.mock('../MetricEntry/MetricEntry', () => jest.fn((props) => (
+  <div data-testid="metric-entry" {...props} />
+)));
 describe('<SystemLoadAverage />', () => {
 
   it('is defined', () => {
     expect(SystemLoadAverage).not.toBeUndefined();
+  });
+
+  it('renders correctly and passes the correct type to MetricEntry', () => {
+    render(<SystemLoadAverage />);
+
+    const metricEntry = screen.getByTestId('metric-entry');
+    expect(metricEntry).toBeInTheDocument();
+
+    expect(metricEntry).toHaveAttribute('type', 'system.load.average.1m');
   });
 
 });
