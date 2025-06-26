67c2d51c3d96b336e24a08d7609449c9de7d0703
sdobbert <dobbert@terrestris.de>
2025-06-26 11:04:03 +0200

    test: add smoke-tests

--- e2e-tests/helpers.ts
+++ e2e-tests/helpers.ts
@@ -1,28 +1,46 @@
+export const switchLanguage = async (page: any, language: string) => {
+  const languageIndicator = !(await page.locator("#root").getByText(language).isVisible());
+  if (languageIndicator) {
+    await page.locator(".language-select").click();
+    await page
+      .locator(".ant-select-item-option-content")
+      .getByText(language, { exact: true })
+      .click();
+  }
+};
+
 export const findElementInPaginatedTable = async (page: any, text: string) => {
   let targetRowLayer;
 
   while (true) {
-    targetRowLayer = await page.locator('.ant-table-row').filter({
-      hasText: text
-    }).first();
+    targetRowLayer = await page
+      .locator(".ant-table-row")
+      .filter({
+        hasText: text,
+      })
+      .first();
 
     if (await targetRowLayer.isVisible()) {
       break;
     }
 
-    const nextPageButton = page.getByRole('listitem', { name: 'Next page', exact: true }).getByRole('button');
+    const nextPageButton = page
+      .getByRole("listitem", { name: "Next page", exact: true })
+      .getByRole("button");
     if (!(await nextPageButton.isEnabled())) {
-      throw new Error(`Element with text "${text}" not found in the table after checking all pages.`);
+      throw new Error(
+        `Element with text "${text}" not found in the table after checking all pages.`
+      );
     }
 
     await nextPageButton.scrollIntoViewIfNeeded();
     await nextPageButton.click();
-    await page.waitForLoadState('networkidle');
-    await page.waitForSelector('.ant-table-row');
+    await page.waitForLoadState("networkidle");
+    await page.waitForSelector(".ant-table-row");
   }
 
   const rowContentLayer = await targetRowLayer.innerText();
-  console.log('Found:', rowContentLayer);
+  console.log("Found:", rowContentLayer);
   return rowContentLayer;
 };
 
@@ -36,9 +54,9 @@ export const deleteAllRowsWithText = async (page: any, text: string) => {
       console.log(`No more rows found with text: "${text}"`);
       break;
     }
-    
-    const matchingRows = await page.locator('.ant-table-row').filter({
-      hasText: text
+
+    const matchingRows = await page.locator(".ant-table-row").filter({
+      hasText: text,
     });
     const rowCount = await matchingRows.count();
 
@@ -46,19 +64,21 @@ export const deleteAllRowsWithText = async (page: any, text: string) => {
 
     for (let i = 0; i < rowCount; i++) {
       const row = matchingRows.first();
-      await row.waitFor({ state: 'visible' });
-      const deleteButton = text.includes('Application')
-      ? row.locator('div svg').nth(1)
-      : row.locator('div svg').first();
+      await row.waitFor({ state: "visible" });
+      const deleteButton = text.includes("Application")
+        ? row.locator("div svg").nth(1)
+        : row.locator("div svg").first();
 
       if (await deleteButton.isVisible()) {
         await deleteButton.click();
-        await page.waitForLoadState('networkidle');
+        await page.waitForLoadState("networkidle");
 
-        await page.getByRole('dialog').getByRole('textbox').fill(text);     
-        await page.getByRole('button', { name: 'OK' }).click();
-        await page.waitForSelector('.ant-notification-notice', { state: 'visible' });
-        await page.waitForLoadState('networkidle');
+        await page.getByRole("dialog").getByRole("textbox").fill(text);
+        await page.getByRole("button", { name: "OK" }).click();
+        await page.waitForSelector(".ant-notification-notice", {
+          state: "visible",
+        });
+        await page.waitForLoadState("networkidle");
       } else {
         console.log(`Delete button for row ${i + 1} is not visible.`);
       }
