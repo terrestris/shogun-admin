a893ee3d74bfe3e16b839f3b74648bcd49e4664d
sdobbert <sdobbert@terrestris.de>
2025-10-21 12:02:21 +0200

    fix: change host for e2e-tests

--- global-setup.ts
+++ global-setup.ts
@@ -6,7 +6,7 @@ async function globalSetup(config: FullConfig) {
   // @ts-ignore
   process.env.ID = '20';
   // @ts-ignore
-  process.env.HOST = 'https://shogun.intranet.terrestris.de';
+  process.env.HOST = 'https://localhost:8080';
 }
 
 export default globalSetup;
