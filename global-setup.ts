d4d79f525a1bdd1d0492f7171594bedd17947c0d
sdobbert <sdobbert@terrestris.de>
2026-07-24 08:32:07 +0200

    fix: make HOST configurable for e2e-tests

--- global-setup.ts
+++ global-setup.ts
@@ -2,11 +2,12 @@ import {
   chromium, FullConfig
 } from '@playwright/test';
 
+const DEFAULT_HOST = 'https://shogun.intranet.terrestris.de';
+
 async function globalSetup(config: FullConfig) {
   // @ts-ignore
   process.env.ID = '20';
-  // @ts-ignore
-  process.env.HOST = 'https://shogun.intranet.terrestris.de';
+  process.env.HOST = process.env.HOST ?? DEFAULT_HOST;
 }
 
 export default globalSetup;
