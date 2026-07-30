3c98318644ebcbf8a7b744649903a0fd36166e50
sdobbert <sdobbert@terrestris.de>
2026-07-30 09:54:43 +0200

    fix: remove unneeded id

--- global-setup.ts
+++ global-setup.ts
@@ -3,11 +3,8 @@ import {
 } from '@playwright/test';
 
 const DEFAULT_HOST = 'https://localhost:8080';
-const DEFAULT_ID = '20';
 
 async function globalSetup(config: FullConfig) {
-  // @ts-ignore
-  process.env.ID = process.env.ID ?? DEFAULT_ID;
   process.env.HOST = process.env.HOST ?? DEFAULT_HOST;
   process.env.ADMIN_LOGIN =  process.env.ADMIN_LOGIN;
   process.env.ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
